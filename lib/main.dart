cc4e8bacb7d8517b5e43f36d1b761ac46ec7d572
deepaksp24 <spdeepak599@gmail.com>
2024-02-14 21:34:04 +0530

    button to get current user location and postion of the user on left of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,27 @@ void main() async {
   runApp(MyApp(initialPosition: initialPosition));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final LatLng initialPosition;
 
-  const MyApp({Key? key, required this.initialPosition}) : super(key: key);
+  MyApp({Key? key, required this.initialPosition}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double userLatitude = 0.0;
+  double userLogitude = 0.0;
+
+  late GoogleMapController mapController;
+
+  void getlocation(double latitude, double longitude) {
+    setState(() {
+      userLatitude = latitude;
+      userLogitude = longitude;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,23 +62,63 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Google Maps Flutter'),
         ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: initialPosition,
-            zoom: 16.0, // Zoom in closer initially
-          ),
-          markers: {
-            Marker(
-              markerId: MarkerId('currentLocation'),
-              position: initialPosition,
-              icon: BitmapDescriptor.defaultMarkerWithHue(
-                  BitmapDescriptor.hueBlue),
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: widget.initialPosition,
+                zoom: 16.0, // Zoom in closer initially
+              ),
+              markers: {
+                Marker(
+                  markerId: MarkerId('currentLocation'),
+                  position: widget.initialPosition,
+                  icon: BitmapDescriptor.defaultMarkerWithHue(
+                      BitmapDescriptor.hueBlue),
+                ),
+              },
+
+              myLocationEnabled: false, // Enable continuous location updates
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+                // Add additional markers or functionality if needed
+              },
             ),
-          },
-          myLocationEnabled: true, // Enable continuous location updates
-          onMapCreated: (GoogleMapController controller) {
-            // Add additional markers or functionality if needed
-          },
+            Positioned(
+              bottom: 40.0,
+              right: 50.0,
+              child: FloatingActionButton(
+                onPressed: () async {
+                  final currentPosition = await Geolocator.getCurrentPosition(
+                    desiredAccuracy: LocationAccuracy.best,
+                    forceAndroidLocationManager: true,
+                  );
+
+                  // userLatitude = currentPosition.latitude;
+                  getlocation(
+                      currentPosition.latitude, currentPosition.longitude);
+                  mapController.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: LatLng(
+                          currentPosition.latitude,
+                          currentPosition.longitude,
+                        ),
+                        zoom: 16.0,
+                      ),
+                    ),
+                  );
+                },
+                child: Icon(
+                  Icons.my_location,
+                ),
+              ),
+            ),
+            Text(
+              '$userLatitude  $userLogitude',
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            )
+          ],
         ),
       ),
     );
